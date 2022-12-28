6d80ded3e9e6f442118e05520ddd10fdf21ddff5
Technicalhassanali <73873109+Technicalhassanali@users.noreply.github.com>
2022-12-28 23:05:21 +0000

    Deploying to gh-pages from @ Technicalhassanali/pdfmcqs@ccee08268ed3b53eb1a82c290c9e51cb8ec84406 🚀

--- my-script.js
+++ my-script.js
@@ -1,5 +1,7 @@
+console.log("Navbar Script Running...");
+
 let navbar = document.querySelector('header .navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 };
-
+console.log("Navbar Script Running End...");
