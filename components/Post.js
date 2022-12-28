a4c9c0cca8a557a9ec6fc9546e062dce7fe614c4
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2022-12-28 15:39:46 +0500

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -6,7 +6,7 @@ export default function Post({ post }) {
     return (
         <>
             <div className='card'>
-                <img src=`/nextjs-blog/{post.frontmatter.cover_image}` alt='' />
+                <img src=`/nextjs-blog/{post.frontmatter.cover_image}` alt='{post.frontmatter.title}' />
                 <div className='post-date'>Posted on {post.frontmatter.date}</div>
                 <span style={{ "color": "red", "fontWeight": "bold", "fontSize": "2rem" }}>{post.frontmatter.category}</span>
                 <h3>{post.frontmatter.title}</h3>
