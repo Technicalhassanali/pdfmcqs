cc8eead7cb147ea3ede824f8f508cd7075bc69e0
Technicalhassanali <73873109+Technicalhassanali@users.noreply.github.com>
2022-12-28 23:00:14 +0000

    Deploying to gh-pages from @ Technicalhassanali/pdfmcqs@5935d3e43d129c84e742b89d1985780fb2bcfbff 🚀

--- my-script.js
+++ my-script.js
@@ -1,22 +1,5 @@
-console.log("Script Running...");
+let navbar = document.querySelector('header .navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+};
 
-// let navbar = document.querySelector('header .navbar');
-// document.querySelector('#menu-btn').onclick = () => {
-//     navbar.classList.toggle('active');
-// };
-
-var accordion = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function () {
-        this.classList.toggle("active-color");
-        var accordion_panel = this.nextElementSibling;
-        if (accordion_panel.style.display === "block") {
-            accordion_panel.style.display = "none";
-        } else {
-            accordion_panel.style.display = "block";
-        }
-    });
-}
-console.log("Script End...");
