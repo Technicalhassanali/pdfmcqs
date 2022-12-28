1711452557d31287d253a8efff57def2db73b27d
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2022-12-28 15:48:13 +0500

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -7,7 +7,7 @@ export default function Post({ post }) {
     return (
         <>
             <div className='card'>
-                <img src=`${url}${post.frontmatter.cover_image}` alt='{post.frontmatter.title}' />
+                <img src={url}{post.frontmatter.cover_image} alt='{post.frontmatter.title}' />
                 <div className='post-date'>Posted on {post.frontmatter.date}</div>
                 <span style={{ "color": "red", "fontWeight": "bold", "fontSize": "2rem" }}>{post.frontmatter.category}</span>
                 <h3>{post.frontmatter.title}</h3>
