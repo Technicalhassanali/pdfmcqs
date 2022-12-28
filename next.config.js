1653f333d2bc1e4c262735b9627360dc7001f195
Hassan Ali <73873109+Technicalhassanali@users.noreply.github.com>
2022-12-28 15:24:30 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
+  basePath: "/nextjs-blog",
+  assetPrefix: "/nextjs-blog",
 };
 
 module.exports = nextConfig;
